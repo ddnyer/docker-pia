ea345f97ac8291d22a8af6b5500672708d588045
Damien Denayer <damien.denayer@gmail.com>
2022-01-02 23:46:47 +0100

    Set region

--- scripts/vpn-startup.sh
+++ scripts/vpn-startup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "Setup PIA VPN"
+
 # Create a TUN device
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
@@ -15,12 +17,23 @@ chown -R docker_user:docker_group /config
 /opt/piavpn/bin/pia-daemon &
 sleep 2
 
+# Set debug logging
+
+piactl set debuglogging $PIA_DEBUG_LOGGING
+
+if [ ! $? -eq 0 ]; then
+    echo "Failed to set PIA log level"
+    exit 5;
+fi
+
 # Log in to PIA
 
-echo -e "$PIA_USERNAME\n$PIA_PASSWORD" > /pia_credentials
+echo -e "$PIA_USERNAME\n$PIA_PASSWORD\n" > /pia_credentials
+cat /pia_credentials
 piactl login /pia_credentials
 
 if [ ! $? -eq 0 ]; then
+    echo "Failed to log in to PIA"
     exit 5;
 fi
 
@@ -29,47 +42,55 @@ fi
 piactl background enable
 
 if [ ! $? -eq 0 ]; then
+    echo "Failed to enable PIA background"
     exit 5;
 fi
 
 # Add dedicated IP
 
+echo "Set dedicate IP"
 if [ ! -z $PIA_DEDICATED_IP_TOKEN ]; then
-    
+
+    echo "Add dedicated IP region"    
     echo -e "$PIA_DEDICATED_IP_TOKEN" > /pia_dedicated_ip_token 
     piactl dedicatedip add /pia_dedicated_ip_token
 
     if [ ! $? -eq 0 ]; then
+        echo "Failed to add dedicated IP"
         exit 5;
     fi
 
 fi
 
-# Set debug logging
-
-piactl set debuglogging $PIA_DEBUG_LOGGING
-
-if [ ! $? -eq 0 ]; then
-    exit 5;
-fi   
-
 piactl set protocol $PIA_PROTOCOL
 
 if [ ! $? -eq 0 ]; then
+    echo "Failed to set PIA protocol"
     exit 5;
 fi
 
 piactl set requestportforward $PIA_PORT_FORWARD
 
 if [ ! $? -eq 0 ]; then
+    echo "Failed to set request port forwarding"
+    exit 5;
+fi
+
+piactl set region $PIA_REGION
+
+if [ ! $? -eq 0 ]; then
+    echo "Failed to set PIA region"
     exit 5;
 fi
 
 # Connect to the VPN
 
+echo "Connect to $(piactl get region)"
+
 piactl connect
 
 if [ ! $? -eq 0 ]; then
+    echo "Failed to connect to PIA VPN"
     exit 5;
 fi
 
